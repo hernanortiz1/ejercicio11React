6d0f642cecd32ab130ed7498098d14c7f0c63db5
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-06-29 11:02:57 -0300

    agregue componentes y baje footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import Formulario from "./components/Formulario";
 
 
 function App() {
@@ -6,7 +6,17 @@ function App() {
 
   return (
     <>
-      
+       <main className="bg-secondary-subtle">
+        <h1 className="my-3 text-center fw-bold">
+          Noticias
+        </h1>
+        <div className="container">
+          <Formulario />
+        </div>
+      </main>
+      <footer className="bg-dark text-light text-center py-3">
+        &copy; Todos los derechos reservados
+      </footer>
     </>
   )
 }
