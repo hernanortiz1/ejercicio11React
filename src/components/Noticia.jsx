3a77e3f7d50811b224cd4f4697d757c436a86745
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-06-29 14:08:25 -0300

    agregue funcion para cuando la descripcion es demasiado extensa

--- src/components/Noticia.jsx
+++ src/components/Noticia.jsx
@@ -4,23 +4,30 @@ const Noticia = ({ noticiaProps }) => {
   return (
     <div className="h-100 my-3 p-md-2">
       <Card className="h-100 d-flex flex-column rounded-3 shadow">
-       <div className="contenedorImgNoticia">
-        <img
-          className="imgNoticia rounded-3"
-          src={noticiaProps.image_url}
-          alt={noticiaProps.title}
-        />
-       </div>
-        
+        <div className="contenedorImgNoticia">
+          <img
+            className="imgNoticia rounded-3"
+            src={noticiaProps.image_url}
+            alt={noticiaProps.title}
+          />
+        </div>
+
         <Card.Body className="flex-grow-1 d-flex flex-column">
           <Card.Title className="fs-3 pb-3">{noticiaProps.title}</Card.Title>
-          <Card.Text className="text-start fs-5">{noticiaProps.description}</Card.Text>
+          <Card.Text className="text-start fs-5">
+            {noticiaProps.description
+              ? noticiaProps.description.substring(0, 200) + "..."
+              : "Sin descripción disponible."}
+          </Card.Text>
         </Card.Body>
-         <div className="card-footer p-2 rounded-2">
-            <a href={noticiaProps.link} className="btn btn-primary w-100 link-light">
+        <div className="card-footer p-2 rounded-2">
+          <a
+            href={noticiaProps.link}
+            className="btn btn-primary w-100 link-light"
+          >
             Saber más
           </a>
-          </div>
+        </div>
       </Card>
     </div>
   );
