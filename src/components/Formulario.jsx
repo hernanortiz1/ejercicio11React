6928195e895bd81d861c6f0d4bada8a75bd2f7c8
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-06-29 12:27:16 -0300

    agregue useEfect y link de api con categoria

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -3,23 +3,26 @@ import ListaNoticias from "./ListaNoticias";
 import { useEffect, useState } from "react";
 
 const Formulario = () => {
+  const [categoria, setCategoria] = useState("");
   const [noticia, setNoticia] = useState({});
 
   useEffect(() => {
-   obtenerNoticia();
-  }, []); 
-  
-    constobtenerNoticia = async () => {
+    if (categoria !== "") {
+      obtenerNoticia();
+    }
+  }, [categoria]);
+
+  constobtenerNoticia = async () => {
     try {
-    //   setMostrarSpinner(true); 
+      //   setMostrarSpinner(true);
       const respuesta = await fetch(
-        "https://thesimpsonsquoteapi.glitch.me/quotes"
+        `https://newsdata.io/api/1/news?apikey=pub_b337209814aa4936a8bbd412055a9faf&category=${categoria}&language=es`
       );
 
       console.log(respuesta);
 
       if (respuesta.status === 200) {
-        const datos = await respuesta.json(); 
+        const datos = await respuesta.json();
         console.log(datos[0]);
         // guardar dentro del state
         setFrase(datos[0]);
@@ -27,7 +30,7 @@ const Formulario = () => {
         // setMostrarSpinner(false);
       }
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   };
 
@@ -39,7 +42,11 @@ const Formulario = () => {
             Seleccione categoria
           </Form.Label>
           <div className="d-flex align-items-center ">
-            <Form.Select required defaultValue="">
+            <Form.Select
+              required
+              value={categoria}
+              onChange={(e) => setCategoria(e.target.value)}
+            >
               <option value="" disabled hidden>
                 Categorias
               </option>
@@ -55,7 +62,7 @@ const Formulario = () => {
         </Form.Group>
       </section>
       <section className="mt-4">
-        <ListaNoticias noticiaProps={noticia}/>
+        <ListaNoticias noticiaProps={noticia} />
       </section>
     </div>
   );
